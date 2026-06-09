9f023f6034bc6a4691f95b2d3855db4198264b04
niyaz <mohemmedniyaz11@gmail.com>
2026-06-09 08:20:37 +0530

    invert tree

--- binarrTree/symentricTree.js
+++ binarrTree/symentricTree.js
@@ -27,3 +27,37 @@ var isSymmetric = function (root) {
     return isMirror(root.left, root.right )
 
 };
+
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function (root) {
+    let queue = [root.left, root.right];
+    while (queue.length) {
+        let ele1 = queue.shift();
+        let ele2 = queue.shift();
+        if (!ele1 && !ele2) {
+            continue
+        }
+        if (!ele1 || !ele2) {
+            return false
+        }
+        if (ele1.val !== ele2.val) {
+            return false
+        }
+        queue.push(ele1.left, ele2.right)
+        queue.push(ele1.right, ele2.left)
+    }
+    return true
+};
